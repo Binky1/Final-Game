daebe8d2f8ad9f304c97cbdf812ddef5d7d45ae4
ori baron <oribaron100@gmail.com>
2023-06-02 21:32:12 +0300

    code better and not messy

--- Player.py
+++ Player.py
@@ -8,27 +8,41 @@ class Player:
         self.size = 120
         self.timeout = 0
 
+        self.maxxpos = 800
 
         self.xpos = xpos
         self.ypos = ypos
         self.health = 100
-        self.sprite = Spritesheet('idle.png')
-        self.idle = [self.sprite.parse_sprite('__Boxing04_Idle_000.png'), self.sprite.parse_sprite('__Boxing04_Idle_001.png'), self.sprite.parse_sprite('__Boxing04_Idle_002.png'),self.sprite.parse_sprite('__Boxing04_Idle_003.png'), self.sprite.parse_sprite('__Boxing04_Idle_004.png'),self.sprite.parse_sprite('__Boxing04_Idle_005.png'), self.sprite.parse_sprite('__Boxing04_Idle_006.png'),  self.sprite.parse_sprite('__Boxing04_Idle_007.png'), self.sprite.parse_sprite('__Boxing04_Idle_008.png'), self.sprite.parse_sprite('__Boxing04_Idle_009.png')]
-        self.sprite = Spritesheet('punchleft.png')
-        self.punchleft = [self.sprite.parse_sprite('__Boxing04_PunchLeft_000.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_001.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_002.png'),self.sprite.parse_sprite('__Boxing04_PunchLeft_003.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_004.png'),self.sprite.parse_sprite('__Boxing04_PunchLeft_005.png')]
-        self.sprite = Spritesheet('walk.png')
-        self.walkright = [self.sprite.parse_sprite('__Boxing04_Walk_000.png'), self.sprite.parse_sprite('__Boxing04_Walk_001.png'), self.sprite.parse_sprite('__Boxing04_Walk_002.png'),self.sprite.parse_sprite('__Boxing04_Walk_003.png'), self.sprite.parse_sprite('__Boxing04_Walk_004.png'),self.sprite.parse_sprite('__Boxing04_Walk_005.png'), self.sprite.parse_sprite('__Boxing04_Walk_006.png'), self.sprite.parse_sprite('__Boxing04_Walk_007.png'), self.sprite.parse_sprite('__Boxing04_Walk_008.png'),self.sprite.parse_sprite('__Boxing04_Walk_009.png')]
-        self.sprite = Spritesheet('walkback.png')
-        self.walkleft = [self.sprite.parse_sprite('__Boxing04_WalkBack_000.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_001.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_002.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_003.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_004.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_005.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_006.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_007.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_008.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_009.png')]
-        self.sprite = Spritesheet('dizzy.png')
-        self.dizzy = [self.sprite.parse_sprite('__Boxing04_Dizzy_000.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_001.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_002.png'),self.sprite.parse_sprite('__Boxing04_Dizzy_003.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_004.png'),self.sprite.parse_sprite('__Boxing04_Dizzy_005.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_006.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_007.png')]
-        
+        # self.sprite = Spritesheet('idle.png')
+        # self.idle = [self.sprite.parse_sprite('__Boxing04_Idle_000.png'), self.sprite.parse_sprite('__Boxing04_Idle_001.png'), self.sprite.parse_sprite('__Boxing04_Idle_002.png'),self.sprite.parse_sprite('__Boxing04_Idle_003.png'), self.sprite.parse_sprite('__Boxing04_Idle_004.png'),self.sprite.parse_sprite('__Boxing04_Idle_005.png'), self.sprite.parse_sprite('__Boxing04_Idle_006.png'),  self.sprite.parse_sprite('__Boxing04_Idle_007.png'), self.sprite.parse_sprite('__Boxing04_Idle_008.png'), self.sprite.parse_sprite('__Boxing04_Idle_009.png')]
+        # self.sprite = Spritesheet('punchleft.png')
+        # self.punchleft = [self.sprite.parse_sprite('__Boxing04_PunchLeft_000.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_001.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_002.png'),self.sprite.parse_sprite('__Boxing04_PunchLeft_003.png'), self.sprite.parse_sprite('__Boxing04_PunchLeft_004.png'),self.sprite.parse_sprite('__Boxing04_PunchLeft_005.png')]
+        # self.sprite = Spritesheet('walk.png')
+        # self.walkright = [self.sprite.parse_sprite('__Boxing04_Walk_000.png'), self.sprite.parse_sprite('__Boxing04_Walk_001.png'), self.sprite.parse_sprite('__Boxing04_Walk_002.png'),self.sprite.parse_sprite('__Boxing04_Walk_003.png'), self.sprite.parse_sprite('__Boxing04_Walk_004.png'),self.sprite.parse_sprite('__Boxing04_Walk_005.png'), self.sprite.parse_sprite('__Boxing04_Walk_006.png'), self.sprite.parse_sprite('__Boxing04_Walk_007.png'), self.sprite.parse_sprite('__Boxing04_Walk_008.png'),self.sprite.parse_sprite('__Boxing04_Walk_009.png')]
+        # self.sprite = Spritesheet('walkback.png')
+        # self.walkleft = [self.sprite.parse_sprite('__Boxing04_WalkBack_000.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_001.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_002.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_003.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_004.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_005.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_006.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_007.png'), self.sprite.parse_sprite('__Boxing04_WalkBack_008.png'),self.sprite.parse_sprite('__Boxing04_WalkBack_009.png')]
+        # self.sprite = Spritesheet('dizzy.png')
+        # self.dizzy = [self.sprite.parse_sprite('__Boxing04_Dizzy_000.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_001.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_002.png'),self.sprite.parse_sprite('__Boxing04_Dizzy_003.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_004.png'),self.sprite.parse_sprite('__Boxing04_Dizzy_005.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_006.png'), self.sprite.parse_sprite('__Boxing04_Dizzy_007.png')]
+        # self.sprite = Spritesheet('block.png')
+        # self.block = [self.sprite.parse_sprite('__Boxing04_Blocking_000.png'), self.sprite.parse_sprite('__Boxing04_Blocking_001.png'), self.sprite.parse_sprite('__Boxing04_Blocking_002.png'),self.sprite.parse_sprite('__Boxing04_Blocking_003.png'), self.sprite.parse_sprite('__Boxing04_Blocking_004.png'),self.sprite.parse_sprite('__Boxing04_Blocking_005.png'), self.sprite.parse_sprite('__Boxing04_Blocking_006.png'), self.sprite.parse_sprite('__Boxing04_Blocking_007.png'), self.sprite.parse_sprite('__Boxing04_Blocking_008.png'), self.sprite.parse_sprite('__Boxing04_Blocking_009.png')]
+        # self.sprite = Spritesheet('ko.png')
+        # self.block = [self.sprite.parse_sprite('__Boxing04_KO_000.png'), self.sprite.parse_sprite('__Boxing04_KO_001.png'), self.sprite.parse_sprite('__Boxing04_KO_002.png'),self.sprite.parse_sprite('__Boxing04_KO_003.png'), self.sprite.parse_sprite('__Boxing04_KO_004.png'),self.sprite.parse_sprite('__Boxing04_KO_005.png'), self.sprite.parse_sprite('__Boxing04_KO_006.png'), self.sprite.parse_sprite('__Boxing04_KO_007.png'), self.sprite.parse_sprite('__Boxing04_KO_008.png'), self.sprite.parse_sprite('__Boxing04_KO_009.png')]
+        self.idle = self.create_sprite_list('Idle', 10)
+        self.punchleft = self.create_sprite_list('PunchLeft', 6)
+        self.walkright = self.create_sprite_list('Walk', 10)
+        self.walkback = self.create_sprite_list('WalkBack', 10)
+        self.dizzy = self.create_sprite_list('Dizzy', 8)
+        self.block = self.create_sprite_list('Blocking', 10)
+        self.ko = self.create_sprite_list('KO', 10)
+
+
         self.runningL = False
         self.runningR = False
 
         self.enemy = enemy
         self.queue = []
 
+        self.state = 'idle'
 
 
         # sprite generators for every movement
@@ -37,9 +51,25 @@ class Player:
         self.idle_generator = self.idle_player()
         self.punchleft_generator = self.punchleft_player()
         self.dizzy_generator = self.dizzy_player()
+        self.block_generator = self.block_player()
 
 
 
+    def create_sprite_list(self, filename, frames) -> list:
+        try:
+            sheet = Spritesheet(f'{filename}.png')
+            return [sheet.parse_sprite(f"__Boxing04_{filename}_{str(i).zfill(3)}.png") for i in range(frames)]
+            # lst = []
+            # for i in range(frames):
+            #     try:
+            #         print(f"__Boxing04_{filename}_{str(i).zfill(3)}")
+            #         sheet.parse_sprite(f"__Boxing04_{filename}_{str(i).zfill(3)}")
+            #     except:
+            #         print("dang it")
+
+        except Exception as e:
+            print(e)
+            return None
     def walk_back(self, screen):
         self.queue = []
         # image = self.get_next(self.walkleft_generator)
@@ -55,10 +85,13 @@ class Player:
         self.runningL = False
         self.runningR = False
     
-    def walkright_sprite(self, screen): 
+    def walkright_sprite(self, x):
         self.queue = []
+        self.maxxpos = x
         self.runningR = True
         self.runningL = False
+
+        # print(kwargs.values())
         # image = self.get_next(self.walkright_generator)
         # image = pygame.transform.scale(image, (299, 299))
         # if self.enemy:
@@ -66,15 +99,15 @@ class Player:
         # screen.blit(image, (self.xpos, self.ypos))
 
     def punch(self, p):
-        if (abs((self.xpos + self.size) - p.xpos) < 130) and not self.enemy:
+        print(p.state)
+        if (abs((self.xpos + self.size) - p.xpos) < 130) and not self.enemy and p.state != 'block':
             if p.health >= 0:
                 print('ee')
                 p.health -= 10
             if p.health > 0 and p.health <= 10:
                 p.dizzy_sprite()
 
-
-        elif (abs((self.xpos - self.size) - p.xpos) < 130) and self.enemy:
+        elif (abs((self.xpos - self.size) - p.xpos) < 130) and self.enemy and p.state != 'block':
             if p.health >= 0:
                 print('hh')
                 p.health -= 10
@@ -82,22 +115,48 @@ class Player:
                 p.dizzy_sprite()
 
     def punchleft_sprite(self, screen, p):
-        self.queue = []
         if self.timeout < 0:
+            self.queue = []
             self.punch(p)
-            self.timeout = 5
-        image = self.get_next(self.punchleft_generator)
+            self.timeout = 35
+            image = self.get_next(self.punchleft_generator)
+            # image = pygame.transform.scale(image, (299, 299))
+            # if self.enemy:
+            #     image = pygame.transform.flip(image,True, False)
+            #
+            #screen.blit(image, (self.xpos, self.ypos))
+            self.queue.append(self.get_next(self.punchleft_generator))
+            self.queue.append(self.get_next(self.punchleft_generator))
+            self.queue.append(self.get_next(self.punchleft_generator))
+            self.queue.append(self.get_next(self.punchleft_generator))
+            self.queue.append(self.get_next(self.punchleft_generator))
+
+
+    def block_sprite(self):
+        self.queue = []
+        image = self.get_next(self.block_generator)
         # image = pygame.transform.scale(image, (299, 299))
         # if self.enemy:
         #     image = pygame.transform.flip(image,True, False)
         #
-        #screen.blit(image, (self.xpos, self.ypos))
-        self.queue.append(self.get_next(self.punchleft_generator))
-        self.queue.append(self.get_next(self.punchleft_generator))
-        self.queue.append(self.get_next(self.punchleft_generator))
-        self.queue.append(self.get_next(self.punchleft_generator))
-        self.queue.append(self.get_next(self.punchleft_generator))
+        # screen.blit(image, (self.xpos, self.ypos))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
 
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
+        self.queue.append(self.get_next(self.block_generator))
     
     def dizzy_sprite(self):
         self.queue = []
@@ -166,22 +225,22 @@ class Player:
 
     def walkleft_player(self):
         i = 0
-        while i < len(self.walkleft):
+        while i < len(self.walkback):
             #screen.blit(self.idle[i], (100, 50))
-            if self.xpos - 3 > 0 and self.enemy == False:
+            if self.xpos +120 - 3 > 0 and self.enemy == False:
                 self.xpos -=3
-            elif self.xpos +  3 < 800 and self.enemy == True:
+            elif self.xpos + 3 < 800 and self.enemy == True:
                 self.xpos +=3
-            yield self.walkleft[i]
-            i = (i + 1) % len(self.walkleft)
+            yield self.walkback[i]
+            i = (i + 1) % len(self.walkback)
 
     def walkright_player(self):
         i = 0
         while i < len(self.walkright):
             #screen.blit(self.idle[i], (100, 50))
-            if self.xpos + 6 < 800 and self.enemy == False:
+            if self.xpos + 6 < self.maxxpos - 100 and self.enemy == False:
                 self.xpos +=4
-            elif self.xpos - 3 > 0 and self.enemy == True:
+            elif self.xpos - 3 > self.maxxpos + 100 and self.enemy == True:
                 self.xpos -=4
             yield self.walkright[i]
             i = (i + 1) % len(self.walkright)
@@ -203,6 +262,15 @@ class Player:
             yield self.dizzy[i]
             i = (i + 1) % len(self.dizzy)
             #print(i)
+
+    def block_player(self):
+        i = 0
+        while i < len(self.block):
+            #screen.blit(self.idle[i], (100, 50))
+            yield self.block[i]
+            i = (i + 1) % len(self.block)
+            #print(i)
+
 
     def idle_player(self):
         i = 0
--- main.py
+++ main.py
@@ -71,12 +71,19 @@ def draw_screen(screen, p, p2):
 
 def parse_protocol(p2, data, screen):
     if data == 'punch':
+        p2.state = 'punch'
         p2.punchleft_sprite(screen, p)
+    elif data == 'block':
+        p2.state = 'block'
+        p2.block_sprite()
     elif data == 'moveright':
-        p2.walkright_sprite(screen)
+        p2.state = 'moveright'
+        p2.walkright_sprite(p.xpos)
     elif data == 'moveleft':
+        p2.state = 'moveleft'
         p2.walk_back(screen)
     elif data == 'idle':
+        p2.state = 'idle'
         p2.key_up()
 
 
@@ -99,22 +106,30 @@ def main():
             elif event.type == KEYDOWN:
                 if event.key == pygame.K_k:
                     state = 'punch'
-                    
+                    p.state = state
                     p.punchleft_sprite(screen, p2)
+                elif event.key == pygame.K_SPACE:
+                    state = 'block'
+                    p.state = state
+                    p.block_sprite()
                 elif event.key == pygame.K_d:
                     state = 'moveright'
-                    p.walkright_sprite(screen)
+                    p.state = state
+                    p.walkright_sprite(p2.xpos)
                 elif event.key == pygame.K_a:
                     state = 'moveleft'
+                    p.state = state
                     p.walk_back(screen)
 
             elif event.type == KEYUP:
                 state = 'idle'
+                p.state = state
                 p.key_up()
 
         send(state)
         if state == 'punch':
             state = 'idle'
+            p.state = state
         draw_screen(screen, p, p2)
         fpsClock.tick(fps)
         frames += 1
